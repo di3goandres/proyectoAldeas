d23948d94d8fe3fb1931cb4b3ef4bb3556865cbb
Diego Andres Montealegre García <diego.montealegre@vasslatam.com>
2020-08-27 15:47:26 -0500

    ajuste para publicar

--- Presupuesto/Aldeas.Web/ClientApp/src/environments/environment.prod.ts
+++ Presupuesto/Aldeas.Web/ClientApp/src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  ApiUrl: "https://sismopro.aldeasinfantiles.org.co:3521/aldeas.presupuesto"
+  ApiUrl: "https://sismopro.aldeasinfantiles.org.co:3521/PresupuestoRest"
 
 };
 
